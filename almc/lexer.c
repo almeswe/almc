12fc179ff3480a4c6af2115862e61fa4e8fc8235
alexey <52577119+almeswe@users.noreply.github.com>
2021-08-30 12:46:42 +0300

    updates added
    -  fixed bug with file reading
    -  fixed bug with first char skipping in get_next_char() function
    -  added keyword recognizing
    -   TokenType enum extended

--- almc/lexer.c
+++ almc/lexer.c
@@ -1,6 +1,44 @@
 #include "lexer.h"
 #include <assert.h>
 
+//TODO: add different operators support!
+
+const char* keywords[] = {
+	"auto",
+	"break",
+	"case",
+	"char",
+	"const",
+	"continue",
+	"default",
+	"double",
+	"enum",
+	"extern",
+	"float",
+	"for",
+	"goto",
+	"if",
+	"int",
+	"long",
+	"register",
+	"return",
+	"short",
+	"signed",
+	"sizeof",
+	"static",
+	"struct",
+	"switch",
+	"typeof",
+	"union",
+	"unsigned",
+	"void",
+	"volatile",
+	"while",
+	"do",
+	"else",
+};
+
+
 Lexer* lexer_new(const char* src, InputStreamType type)
 {
 	Lexer* lex = calloc(1, sizeof(Lexer));
@@ -53,15 +91,13 @@ Token* lexer_get_tokens(Lexer* lex)
 		else if (isidnt(curr_char))
 			sbuffer_add(tokens, *get_idnt_token(lex));
 		//todo: else if reserved
-		else if (isspace(curr_char))
-			get_next_char(lex);
 		else
 		{
 			printf("This character [{'%c'} line: %d, pos: %d] is not supported by lexer!\n", get_curr_char(lex), lex->curr_line, lex->curr_line_offset);
 			get_next_char(lex);
 		}
 	}	
-	return tokens;
+	return lex->tokens = tokens;
 }
 
 char get_next_char(Lexer* lex)
@@ -71,25 +107,24 @@ char get_next_char(Lexer* lex)
 		return '\0';
 	else
 	{
-		//todo: add full escape sequence table??
-		//todo: first char is skipped, fix this
-		switch (ch = get__next_char(lex))
+		switch (ch = get_curr_char(lex))
 		{
 		case '\n':
-			lex->curr_line++;
+			lex->curr_line += 1;
 			break;
 		case '\r':
-			lex->curr_line_offset = 0;
+			lex->curr_line_offset = 1;
 			break;
 		case '\v': //???
 		case '\t':
 			break;
-		//todo: add comment support
+		//todo: add escape chars support
+		//todo: comment logic occures bug with 
 		//todo: add preprocessor instead? 
 		case '/':
 		{
-			//todo: + escape chars here
-			ch = get_next_char(lex);
+			//todo: may be usafe using get__next_char()
+			ch = get__next_char(lex);
 			switch (ch)
 			{
 			case '/':
@@ -103,13 +138,14 @@ char get_next_char(Lexer* lex)
 				lex->curr_line_offset += 2;
 				break;
 			}
+			break;
 		}
 		default:
 			lex->curr_line_offset++;
 			break;
 		}
 	}
-	return ch;
+	return get__next_char(lex);
 }
 
 Token* get_next_token()
@@ -125,7 +161,7 @@ Token* get_num_token(Lexer* lex)
 
 	while (isdigit(get_next_char(lex)))
 	{
-		//todo: float point support
+		//TODO: float point support
 		//todo: int32 and int64 token support
 		int_value *= 10;
 		int_value += get_curr_char(lex) - '0';
@@ -136,15 +172,8 @@ Token* get_num_token(Lexer* lex)
 		size++;
 	}
 
-	Token* token = token_new(
-		TOKEN_NUM,
-		src_context_new(
-			"undefined",
-			lex->curr_line_offset,
-			size,
-			lex->curr_line
-		)
-	);
+	Token* token = token_new(TOKEN_NUM, 
+		src_context_new("undefined", lex->curr_line_offset, size, lex->curr_line));
 	token->int_value = int_value;
 	return token;
 }
@@ -153,6 +182,7 @@ Token* get_idnt_token(Lexer* lex)
 {
 	size_t size = 1;
 	char* value = NULL;
+	Token* token = NULL;
 	sbuffer_add(value, get_curr_char(lex));
 
 	while (isidnt_ext(get_next_char(lex)))
@@ -162,45 +192,50 @@ Token* get_idnt_token(Lexer* lex)
 	}
 	sbuffer_add(value, '\0');
 
-	Token* token = token_new(
-		TOKEN_IDNT,
-		src_context_new(
-			"undefined",
-			lex->curr_line_offset,
-			size,
-			lex->curr_line
-		)
-	);
+	int order = iskeyword(value);
+	token = (order > 0) ? get_keyword_token(lex, order) :
+		 token_new(TOKEN_IDNT, src_context_new("undefined", lex->curr_line_offset, size, lex->curr_line));
 	token->idnt_value = sbuffer_rdc(value, sbuffer_len(value));
 	return token;
 }
 
+Token* get_keyword_token(Lexer* lex, int order)
+{
+	const char* keyword = keywords[order];
+	return token_new(order + KEYWORD_IN_TOKEN_ENUM_OFFSET,
+		src_context_new("undefined", lex->curr_line_offset, strlen(keyword), lex->curr_line));
+}
+
 void multi_line_comment(Lexer* lex)
 {
-	char curr = get__next_char(lex);
+	char curr = get_next_char(lex);
 	while (!is_stream_empty(lex))
 	{
 		if (curr == '*')
 		{
-			curr = get__next_char(lex);
+			curr = get_next_char(lex);
 			if (curr == '/')
 				break;
 		}
 		else
-			curr = get__next_char(lex);
+			curr = get_next_char(lex);
 	}
 }
 
 void single_line_comment(Lexer* lex)
 {
-	char curr = get__next_char(lex);
+	char curr = get_next_char(lex);
 	while (!is_stream_empty(lex) && curr != '\n')
-		curr = get__next_char(lex);
+		curr = get_next_char(lex);
+	//todo: wrong
+	if (curr == '\n')
+		get_next_char(lex);
 }
 
 int fgetc_ext(FILE* file)
 {
-	return fseek(file, 0, SEEK_CUR);
+	fseek(file, -1, SEEK_CUR);
+	return fgetc(file);
 }
 
 inline int isidnt(char ch)
@@ -211,7 +246,16 @@ inline int isidnt_ext(char ch)
 {
 	return isidnt(ch) || isdigit(ch);
 }
-inline int isreserved(char ch)
+inline int isdigit_ext(char ch)
 {
+	return isdigit(ch) || ch == '_';
+}
 
+//returns -1 if idnt is not the, either returns the order in keywords array
+inline int iskeyword(const char* idnt)
+{
+	for (int i = 0; i < KEYWORDS; i++)
+		if (strcmp(idnt, keywords[i]) == 0)
+			return i;
+	return -1;
 }
--- almc/lexer.h
+++ almc/lexer.h
@@ -1,12 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
 #include <ctype.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <string.h>
 #include "sbuffer.h"
 
-//todo: is needed?	
 #ifndef LEXER_STREAM_MACROS
 #define LEXER_STREAM_MACROS
 
@@ -15,17 +14,83 @@
 #define get__next_char(lex) ((lex->type == STREAM_FILE) ? get__next_char_fstream(lex) : get__next_char_cstream(lex))
 
 #define get__curr_char_cstream(lex) (*lex->char_stream)
-#define get__curr_char_fstream(lex) (fseek(lex->file_stream, 0, SEEK_CUR))
+#define get__curr_char_fstream(lex) (fgetc_ext(lex->file_stream))
 
 #define get_curr_char(lex) ((lex->type == STREAM_FILE) ? get__curr_char_fstream(lex) : get__curr_char_cstream(lex))
 #define is_stream_empty(lex) ((lex->type == STREAM_FILE) ? (feof(lex->file_stream)) : (*lex->char_stream == '\0'))
 
 #endif
 
+#define KEYWORDS 32
+#define KEYWORD_IN_TOKEN_ENUM_OFFSET TOKEN_IDNT + 1
+
+const char chars[];
+const char* keywords[];
+
 typedef enum
 {
+	TOKEN_PLUS,
+	TOKEN_DASH,
+	TOKEN_ASTERISK,
+	TOKEN_SLASH,
+	TOKEN_MODULUS,
+	TOKEN_BAR,
+	TOKEN_TILDE,
+	TOKEN_CARET,
+	TOKEN_S_QUOTE,
+	TOKEN_D_QUOTE,
+	TOKEN_EXCL_MARK,
+	TOKEN_COMMA,
+	TOKEN_COLON,
+	TOKEN_SEMICOLON,
+	TOKEN_ASSIGN,
+	TOKEN_QUESTION,
+	TOKEN_AMPERSAND,
+	TOKEN_APOSTROPHE,
+	TOKEN_CL_PAREN,
+	TOKEN_OP_PAREN,
+	TOKEN_CL_BRACKET,
+	TOKEN_OP_BRACKET,
+	TOKEN_CL_BRACE,
+	TOKEN_OP_BRACE,
+	TOKEN_LEFT_ANGLE,
+	TOKEN_RIGHT_ANGLE,
+
 	TOKEN_NUM,
 	TOKEN_IDNT,
+
+	TOKEN_KEYWORD_AUTO,
+	TOKEN_KEYWORD_BREAK,
+	TOKEN_KEYWORD_CASE,
+	TOKEN_KEYWORD_CHAR,
+	TOKEN_KEYWORD_CONST,
+	TOKEN_KEYWORD_CONTINUE,
+	TOKEN_KEYWORD_DEFAULT,
+	TOKEN_KEYWORD_DOUBLE,
+	TOKEN_KEYWORD_ENUM,
+	TOKEN_KEYWORD_EXTERN,
+	TOKEN_KEYWORD_FLOAT,
+	TOKEN_KEYWORD_FOR,
+	TOKEN_KEYWORD_GOTO,
+	TOKEN_KEYWORD_IF,
+	TOKEN_KEYWORD_INT,
+	TOKEN_KEYWORD_LONG,
+	TOKEN_KEYWORD_REGISTER,
+	TOKEN_KEYWORD_RETURN,
+	TOKEN_KEYWORD_SHORT,
+	TOKEN_KEYWORD_SIGNED,
+	TOKEN_KEYWORD_SIZEOF,
+	TOKEN_KEYWORD_STATIC,
+	TOKEN_KEYWORD_STRUCT,
+	TOKEN_KEYWORD_SWITCH,
+	TOKEN_KEYWORD_TYPEOF,
+	TOKEN_KEYWORD_UNION,
+	TOKEN_KEYWORD_UNSIGNED,
+	TOKEN_KEYWORD_VOID,
+	TOKEN_KEYWORD_VOLATILE,
+	TOKEN_KEYWORD_WHILE,
+	TOKEN_KEYWORD_DO,
+	TOKEN_KEYWORD_ELSE,
 } TokenType;
 
 typedef struct
@@ -41,6 +106,7 @@ typedef struct
 	union
 	{
 		uint64_t int_value;
+		double float_value;
 		const char* op_value;
 		const char* idnt_value;
 	};
@@ -65,8 +131,8 @@ typedef struct
 	InputStreamType type;
 	union
 	{
-		const char* char_stream;
 		FILE* file_stream;
+		const char* char_stream;
 	};
 } Lexer;
 
@@ -83,5 +149,4 @@ char get_next_char(Lexer* lex);
 Token* get_next_token();
 Token* get_num_token(Lexer* lex);
 Token* get_idnt_token(Lexer* lex);
-
-int fgetc_ext(FILE* file);
+Token* get_keyword_token(Lexer* lex, int order);
