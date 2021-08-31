08b85aa4963ad5c505007a41403fb8b16c22de8e
alexey <52577119+almeswe@users.noreply.github.com>
2021-08-31 22:12:52 +0300

    new tests for lexer added

--- almc/main.c
+++ almc/main.c
@@ -1,41 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <assert.h>
 #include "lexer.h"
 #include "sbuffer.h"
 
 #define lexer_test_case_init(lexer, tokens, type, src) lexer = lexer_new(src, type), tokens = lexer_get_tokens(lexer) 
 
-void lex_test()
+void print_tokens(Token* const tokens)
+{
+	for (int i = 0; i < sbuffer_len(tokens); i++)
+		printf("Token: %d, type: %d (line: %d, pos: %d, by %d)\n",
+			i+1, tokens[i].type, tokens[i].context->line, tokens[i].context->start, tokens[i].context->size);
+}
+
+void lexer_test()
 {
 	Lexer* lexer;
 	Token* tokens;
 	const char* stream;
-	
-	lexer_test_case_init(lexer, tokens, STREAM_FILE, "C:\\Users\\HP\\source\\repos\\almc\\Debug\\test.txt");
+
+	// number lexing test
+	lexer_test_case_init(lexer, tokens, STREAM_CHAR_PTR, "123 0b01010 0xAbCdEf 0.123 0xFFFFFFFFFFFFFFFF 0b1111111111111111111111111111111111111111111111111111111111111111 0x0 0b0");
+	assert(sbuffer_len(tokens) == 8);
+	assert(tokens[0].ivalue == 123);
+	assert(tokens[1].ivalue == 0b01010);
+	assert(tokens[2].ivalue == 0xabcdef);
+	assert(tokens[4].ivalue == ULLONG_MAX);
+	assert(tokens[5].ivalue == ULLONG_MAX);
+	assert(tokens[6].ivalue == 0);
+	assert(tokens[7].ivalue == 0);
+	//todo: occurs bug
+	//assert(tokens[3].ivalue == 0.123);
+	sbuffer_free(tokens);
+
+	// different operators (keychars) lexing test
+	lexer_test_case_init(lexer, tokens, STREAM_CHAR_PTR, " ^<< <<=\n ++\r -- != ) (= &&");
 	assert(sbuffer_len(tokens) == 10);
-	assert(tokens[2].type == TOKEN_SIZEOF);
-	assert(tokens[3].type == TOKEN_FOR);
-	assert(tokens[4].type == TOKEN_STRUCT);
-	assert(tokens[5].type == TOKEN_REGISTER);
+	print_tokens(tokens);
 	sbuffer_free(tokens);
 
-	lexer_test_case_init(lexer, tokens, STREAM_FILE, "C:\\Users\\HP\\source\\repos\\almc\\Debug\\test1.txt");
-	assert(sbuffer_len(tokens) == 6);
+	// file + keywords test
+	lexer_test_case_init(lexer, tokens, STREAM_FILE, "C:\\Users\\HP\\source\\repos\\almc\\Debug\\test.txt");
+	assert(sbuffer_len(tokens) == 14);
+	assert(tokens[4].type == TOKEN_KEYWORD_SIZEOF);
+	assert(tokens[5].type == TOKEN_KEYWORD_FOR);
+	assert(tokens[6].type == TOKEN_KEYWORD_STRUCT);
+	assert(tokens[7].type == TOKEN_KEYWORD_REGISTER);
 	sbuffer_free(tokens);
 
-	lexer_test_case_init(lexer, tokens, STREAM_CHAR_PTR, "//__AashdA123  ! as.asda_\r\n 11 ");
-	assert(sbuffer_len(tokens) == 1);
-	assert(tokens[0].int_value == 11);
+	// multi comment test?
+	lexer_test_case_init(lexer, tokens, STREAM_CHAR_PTR, "/*__AashdA123  ! as.*/!asda_\r\n 11 ");
+	assert(sbuffer_len(tokens) == 3);
+	assert(tokens[0].type == TOKEN_EXCL_MARK);
+	assert(strcmp(tokens[1].str_value, "asda_") == 0);
+	assert(tokens[2].ivalue == 11);
 	sbuffer_free(tokens);
 
+	// single comment test?
 	lexer_test_case_init(lexer, tokens, STREAM_CHAR_PTR, "//__AashdA123  ! as.asda_ 11 ");
 	assert(sbuffer_len(tokens) == 0);
 	sbuffer_free(tokens);
 
-	lexer_test_case_init(lexer, tokens, STREAM_CHAR_PTR, "__AashdA123  ! as.asda_ 132123  ");
-	assert(sbuffer_len(tokens) == 4);
-	assert(strcmp(tokens[0].idnt_value, "__AashdA123") == 0);
-	assert(strcmp(tokens[2].idnt_value, "asda_") == 0);
-	assert(tokens[3].int_value == 132123);
+	// first lexing test, for primitives
+	lexer_test_case_init(lexer, tokens, STREAM_CHAR_PTR, "__AashdA123  %  &as.asda_ 132123  ");
+	assert(sbuffer_len(tokens) == 6);
+	assert(strcmp(tokens[0].str_value, "__AashdA123") == 0);
+	assert(strcmp(tokens[4].str_value, "asda_") == 0);
+	assert(tokens[1].type == TOKEN_MODULUS);
+	assert(tokens[2].type == TOKEN_AMPERSAND);
+	assert(tokens[5].ivalue == 132123);
 	sbuffer_free(tokens);
 }
 void sb_test()
@@ -70,26 +103,15 @@ void sb_test()
 	sbuffer_free(str);
 }
 
-void separate_test()
-{
-	FILE* file;
-	fopen_s(&file, "C:\\Users\\HP\\source\\repos\\almc\\Debug\\test.txt", "rb");
-	assert(file);
-	
-	//todo: somekind of undo here
-	char c = fseek(file, -1, SEEK_CUR);
-	c = fgetc(file);
-}
-
 void test()
 {
-	//separate_test();
 	sb_test();
-	lex_test();
+	lexer_test();
 }
 
 int main(int argc, char** argv)
 {
 	test();
+	system("pause");
 	return 0;
 }
