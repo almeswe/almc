4ff436bc75cfdcda8f8a4d2ca0fdbd8303a78da3
alexey <52577119+almeswe@users.noreply.github.com>
2021-09-05 20:07:35 +0300

    context replaced in its own file (for reuse)

---
+++ almc/context.h
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+#ifndef CONTEXT_H
+#define CONTEXT_H
+
+typedef struct
+{
+	uint32_t size;
+	uint32_t line;
+	uint32_t start;
+	const char* file;
+} SrcContext;
+
+#endif
+
+const char* src_context_tostr(SrcContext* context);
+SrcContext* src_context_new(const char* file, uint32_t start, uint32_t size, uint32_t line);
