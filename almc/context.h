0290b331cf082aa818b466fac9ec1311d76bc5b2
alexey <52577119+almeswe@users.noreply.github.com>
2021-09-12 18:11:41 +0300

    updates for common file
    - added console color macroses

--- almc/context.h
+++ almc/context.h
@@ -1,8 +1,8 @@
+#ifndef ALMC_CONTEXT_H
+#define ALMC_CONTEXT_H
+
 #include <stdint.h>
 
-#ifndef CONTEXT_H
-#define CONTEXT_H
- 
 typedef struct SrcContext
 {
 	uint32_t size;
@@ -11,7 +11,7 @@ typedef struct SrcContext
 	const char* file;
 } SrcContext;
 
-#endif
-
 const char* src_context_tostr(SrcContext* context);
 SrcContext* src_context_new(const char* file, uint32_t start, uint32_t size, uint32_t line);
+
+#endif
