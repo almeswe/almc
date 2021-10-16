4e614c675a6a90b9fa57a9bc19cc68caf5bd3d86
alexey <armag2003@mail.ru>
2021-10-17 00:16:04 +0300

    updates for os
    - rewrited path_combine with _CRT_SECURE_NO_WARNINGS (to fix bug with fopen_s)
    - added function for extractiong disk name from path

--- almc/src/utils/os.c
+++ almc/src/utils/os.c
@@ -17,6 +17,18 @@ char* get_curr_dir()
 	return dir;
 }
 
+char* get_root(const char* path)
+{
+	char* disk = (char*)malloc(sizeof(char) * 4);
+	if (!disk)
+		return NULL;
+	disk[0] = path[0];
+	disk[1] = ':';
+	disk[2] = '\\';
+	disk[3] = '\0';
+	return disk;
+}
+
 char* get_dir_parent(const char* dir)
 {
 	size_t dir_name_len = 0;
@@ -38,30 +50,32 @@ char* path_combine(const char* parent, const char* child)
 {
 	size_t child_size = strlen(child);
 	size_t parent_size = strlen(parent);
-	size_t combined_size = child_size + parent_size + 2; // +2 -> 1 for separator & 1 for \0
-	char* combined = (char*)malloc((parent_size +
-		child_size + 2) * sizeof(char)); 
+	char ends_with_sep =
+		parent[parent_size - 1] == WIN_PATH_SEP;
+	size_t combined_size = child_size + parent_size + 
+		1 + !ends_with_sep; // +2 -> 1 for separator & 1 for \0
+	char* combined = (char*)malloc(combined_size * sizeof(char)); 
 	if (!combined)
 		return NULL;
-	strcpy_s(combined, combined_size, parent);
-	char ends_with_sep = 
-		parent[parent_size - 1] == WIN_PATH_SEP;
+	strcpy(combined, parent);
 	if (!ends_with_sep)
 		combined[parent_size] = WIN_PATH_SEP;
-	strcpy_s(combined + parent_size + !ends_with_sep,
-		 combined_size, child);
+	strcpy(combined + parent_size + !ends_with_sep, child);
 	return combined;
 }
 
 int dir_exists(const char* dir)
 {
-	return GetFileAttributesA(dir) == 
+	return GetFileAttributes(dir) == 
 		FILE_ATTRIBUTE_DIRECTORY;
 }
 
 int file_exists(const char* file)
 {
-	DWORD dwAttrib = GetFileAttributesA(file);
-	return (dwAttrib != INVALID_FILE_ATTRIBUTES &&
-		!(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+	FILE* f = 0;
+	fopen_s(&f, file, "r");
+	if (!f)
+		return 0;
+	fclose(f);
+	return 1;
 }
--- almc/src/utils/os.h
+++ almc/src/utils/os.h
@@ -2,12 +2,15 @@
 #define ALMC_OS_H
 
 #define WIN32_LEAN_AND_MEAN
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 
 char* get_drives();
 char* get_curr_dir();
+char* get_root(const char* path);
 char* get_dir_parent(const char* dir);
 
 char* path_combine(const char* parent, const char* child);
@@ -15,4 +18,5 @@ char* path_combine(const char* parent, const char* child);
 int dir_exists(const char* dir);
 int file_exists(const char* file);
 
+#undef _CRT_SECURE_NO_WARNINGS
 #endif
