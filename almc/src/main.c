1d6665dd12433ddd98f0801186f30158ab664d70
alexey <52577119+almeswe@users.noreply.github.com>
2021-11-06 21:54:30 +0300

    Update main.c

--- almc/src/main.c
+++ almc/src/main.c
@@ -13,9 +13,8 @@ int test()
 		Parser* parser = parser_new(path_copy, lex(lexer));
 		AstRoot* ast = parse(parser);
 		
-		StackFrame* frame = stack_frame_new(ast->stmts[0]->func_decl);
-		//for (int i = 0; i < sbuffer_len(ast->stmts); i++)
-		gen_func_decl(ast->stmts[0]->func_decl, frame);
+		for (int i = 0; i < sbuffer_len(ast->stmts); i++)
+			gen_stmt(ast->stmts[i], NULL);
 		//print_expr(expr, "");
 		//init_reserved_registers();
 		char c = getchar();
@@ -23,9 +22,58 @@ int test()
 	}
 }
 
+int type_test()
+{
+	while (1)
+	{
+		char buffer[50];
+		char* input = gets(buffer);
+		Lexer* lexer = lexer_new(
+			input, FROM_CHAR_PTR);
+		Parser* parser = parser_new(NULL, lex(lexer));
+		Expr* expr = parse_expr(parser);
+
+		print_type(get_expr_type(expr), "");
+
+		lexer_free(lexer);
+		parser_free(parser);
+		expr_free(expr);
+		clear_imported_modules();
+	}
+}
+
+int cast_test()
+{
+	char* types[] = {
+		"u8",
+		"i8",
+		"chr",
+		"u16",
+		"i16",
+		"u32",
+		"i32",
+		"u64",
+		"i64",
+		"f32",
+		"f64"
+	};
+	Type* type = cnew_s(Type, type, 1);
+	Type* type1 = cnew_s(Type, type1, 1);
+
+	for (int i = 0; i < 11; i++)
+		for (int j = 0; j < 11; j++)
+		{
+			type->repr = types[i];
+			type1->repr = types[j];
+			printf("Cast %s to %s is %d\n", type1->repr, type->repr, can_cast_implicitly(type, type1));
+		}
+}
+
 int main(int argc, char** argv)
 {
-	test();
+	//test();
+	//cast_test();
+	type_test();
 	run_tests();
 	return 0;
 }
