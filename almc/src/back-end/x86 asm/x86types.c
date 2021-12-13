ce6933de41fd1a5e2b20ca182be4714f7f99296b
alexey <52577119+almeswe@users.noreply.github.com>
2021-12-14 01:23:00 +0300

    replaced all useless code from x86types

--- almc/src/back-end/x86 asm/x86types.c
+++ almc/src/back-end/x86 asm/x86types.c
@@ -9,65 +9,22 @@ static const char* predefined_types_prefixes[] = {
 	"REAL8",
 };
 
-int get_type_prefix(Type* type)
+char* get_ptr_prefix(Type* type)
 {
-	/*if (type->spec.ptr_rank)
-		return DWORD;
-	if (!type->spec.is_predefined)
+	if (IS_AGGREGATE_TYPE(type))
 		assert(0);
 	else
 	{
-		if (strcmp(type->repr, "i8") == 0 ||
-			strcmp(type->repr, "u8") == 0 ||
-			strcmp(type->repr, "chr") == 0)
-			return BYTE;
-		else if (strcmp(type->repr, "i16") == 0 ||
-			strcmp(type->repr, "u16") == 0)
-			return WORD;
-		else if (strcmp(type->repr, "i32") == 0 ||
-			strcmp(type->repr, "u32") == 0 ||
-			strcmp(type->repr, "f32") == 0 ||
-			strcmp(type->repr, "str") == 0)
-			return DWORD;
-		else if (strcmp(type->repr, "i64") == 0 ||
-			strcmp(type->repr, "u64") == 0 ||
-			strcmp(type->repr, "f64") == 0)
-			return QWORD;
-		else
-			assert(0);
-	}*/
-	assert(0);
-}
-
-const char* get_predefined_type_str(int prefix)
-{
-	assert(prefix >= BYTE && prefix <= REAL8);
-	return predefined_types_prefixes[prefix];
-}
-
-int get_type_size2(Type* type)
-{
-	assert(0);
-	/*if (type->spec.ptr_rank)
-		return 4;
-	if (!type->spec.is_predefined)
-		assert(0);
-	else
-	{
-		switch (get_type_prefix(type))
+		switch (type->size)
 		{
-		case BYTE:
-			return 1;
-		case WORD:
-			return 2;
-		case DWORD:
-		case REAL4:
-			return 4;
-		case QWORD:
-		case REAL8:
-			return 8;
+		case I8_SIZE:
+			return "byte";
+		case I16_SIZE:
+			return "word";
+		case I32_SIZE:
+			return is_real_type(type) ?
+				"real4" : "dword";
 		}
 	}
 	assert(0);
-	return -1;*/
 }
--- almc/src/back-end/x86 asm/x86types.h
+++ almc/src/back-end/x86 asm/x86types.h
@@ -1,18 +1,9 @@
 #ifndef ALMC_BACK_END_TYPES
 #define ALMC_BACK_END_TYPES
 
-#define REAL4 4
-#define REAL8 5
+#include <assert.h>
+#include "..\..\front-end\type.h"
 
-#define BYTE 0
-#define WORD 1
-#define DWORD 2
-#define QWORD 3
-
-#include "..\..\front-end\ast\ast.h"
-
-int get_type_size2(Type* type);
-int get_type_prefix(Type* type);
-const char* get_predefined_type_str(int prefix);
+char* get_ptr_prefix(Type* type);
 
 #endif
