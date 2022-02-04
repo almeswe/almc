2f09be51c8c891e66e46bcc36ad77f6d20f4f7e7
alexey <52577119+almeswe@users.noreply.github.com>
2022-02-04 17:52:41 +0300

    Update instructions_gen.py

--- almc/src/back-end/x86/instructions_gen.py
+++ almc/src/back-end/x86/instructions_gen.py
@@ -1,12 +1,12 @@
-with open('instructions_list.txt', 'r') as file:
+with open('sse_instructions_list.txt', 'r') as file:
     i: int = 0
     lines = []
     for line in file:
         lines.append(line[:-1])
     for line in lines:
-        print(f'#define\t{line.upper()}\t{i}')
+        print(f'#define\t{line.upper()}\t\t{i}')
         i += 1
-    print('\nchar* instructions_str[] = {')
+    print('\nstatic char* instructions_str[] = {')
     for line in lines:
-        print(f'\t[{line.upper()}]\t= \"{line}\",')
-    print('}')
+        print(f'\t[{line.upper()}]\t\t= \"{line.lower()}\",')
+    print('};')
